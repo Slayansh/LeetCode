56f1ef0775b67eb20b467430a96311e3e41fbe84
Slayansh <shreyanshmathur.2005@gmail.com>
2026-07-01 11:36:22 +0530

    Added solution - LeetHub

---
+++ 700490-top-view-of-binary-tree-GFG/700490-top-view-of-binary-tree.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        vector<int> ans;
+        if (!root) return ans;
+
+        map<int, int> mp;
+        queue<pair<Node*, int>> q;
+
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto front = q.front();
+            q.pop();
+
+            Node* node = front.first;
+            int hd = front.second;
+
+            if (mp.find(hd) == mp.end())
+                mp[hd] = node->data;
+
+            if (node->left)
+                q.push({node->left, hd - 1});
+
+            if (node->right)
+                q.push({node->right, hd + 1});
+        }
+
+        for (auto &it : mp)
+            ans.push_back(it.second);
+
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
