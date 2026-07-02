8ff09a40981730abb7399c6e6638ed4d3cde312d
Slayansh <shreyanshmathur.2005@gmail.com>
2026-07-02 11:30:35 +0530

    Time: 0 ms (100%), Space: 18.5 MB (27.7%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isMirror(TreeNode* left, TreeNode* right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+
+        if (left->val != right->val)
+            return false;
+
+        return isMirror(left->left, right->right) &&
+               isMirror(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root->left, root->right);
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
